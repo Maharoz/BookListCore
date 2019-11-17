523e2ba10f93717d9a98abb23f1ffa81014323af
maharoz <mahruj66@gmail.com>
2019-11-17 20:28:52 +0600

    create page ui design done

--- BookList/Model/Book.cs
+++ BookList/Model/Book.cs
@@ -14,5 +14,7 @@ namespace BookList.Model
         [Required]
         public string Name { get; set; }
         public string Author { get; set; }
+
+        public string ISBN { get; set; }
     }
 }
